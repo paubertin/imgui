d1a702decb6736505b105062a3700757bfd5285e
scal <paubertin@gmail.com>
2023-04-16 16:51:05 +0200

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -11,7 +11,7 @@ project "ImGui"
     "imgui.h",
     "imgui.cpp",
     "imgui_draw.cpp",
-    "imgui_internal.cpp",
+    "imgui_internal.h",
     "imgui_widgets.cpp",
     "imgui_rectpack.h",
     "imgui_textedit.h",
