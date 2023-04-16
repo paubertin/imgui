46d346d327532621cf70889db9a0560552799c31
scal <paubertin@gmail.com>
2023-04-16 16:50:29 +0200

    add premake5

---
+++ premake5.lua
@@ -0,0 +1,28 @@
+project "ImGui"
+  king "StaticLib"
+  language "C++
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+  files
+  {
+    "imconfig.h",
+    "imgui.h",
+    "imgui.cpp",
+    "imgui_draw.cpp",
+    "imgui_internal.cpp",
+    "imgui_widgets.cpp",
+    "imgui_rectpack.h",
+    "imgui_textedit.h",
+    "imgui_truetype.h",
+    "imgui_demo.cpp",
+  }
+
+	filter "system:windows"
+		systemversion "latest"
+    cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:release"
+		buildoptions "/MT"
